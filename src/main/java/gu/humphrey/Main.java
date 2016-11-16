1faa43cf136b5d1a07d422328a8bb7c2f9f6b46b
Huaping Gu <hpgu@apple.com>
2016-11-16 13:46:33 -0800

    print out message debug

--- src/main/java/gu/humphrey/Main.java
+++ src/main/java/gu/humphrey/Main.java
@@ -23,36 +23,26 @@ public class Main {
     get("/hello", (req, res) -> "Hello World");
 
       get("/hook", (req, res) -> {
-          Set<String> headers = req.headers();
-          System.out.println("headers=" + req.headers());
-          headers.forEach((v)->{
-                      System.out.println(v+"="+req.headers(v));
+                  if ("subscribe".equals(req.queryParams("hub.mode"))) {
+                      if ("1234567890".equals(req.queryParams("hub.verify_token"))) {
+                          System.out.println("Validating webhook");
+                          res.status(HttpServletResponse.SC_OK);
+                          return req.queryParams("hub.challenge");
+                      } else {
+                          System.out.println("Failed validation. Make sure the validation tokens match.");
+                          res.status(HttpServletResponse.SC_FORBIDDEN);
+                          return "token code not match challenge code";
+                      }
                   }
-          );
 
-          System.out.println("attributes=" + req.attributes());
-          System.out.println("body=" + req.body());
-          System.out.println("cookies="+req.cookies());
-          System.out.println("host="+req.host());
-          System.out.println("params=" + req.params());
-          System.out.println("queryString="+req.queryString());
-          System.out.println("queryMap="+req.queryMap());
-          System.out.println("queryMap="+req.queryMap().toMap());
-          System.out.println("queryParams="+req.queryParams());
-          System.out.println("hub.mode="+req.queryParams("hub.mode")
-                  + "; hub.verify_token="+req.queryParams("hub.verify_token")
-                  + "; hub.challenge="+req.queryParams("hub.challenge"));
-          if ("subscribe".equals(req.queryParams("hub.mode")) &&
-                  "1234567890".equals(req.queryParams("hub.verify_token"))) {
-              System.out.println("Validating webhook");
-              res.status(HttpServletResponse.SC_OK);
-              return req.queryParams("hub.challenge");
-          } else {
-              System.out.println("Failed validation. Make sure the validation tokens match.");
-              res.status(HttpServletResponse.SC_FORBIDDEN);
-              return "token code not match challenge code";
-          }
-      });
+                  String body = req.body();
+                  System.out.println("body:"+body);
+
+                  return "";
+              }
+
+
+      );
 
     get("/", (request, response) -> {
             Map<String, Object> attributes = new HashMap<>();
