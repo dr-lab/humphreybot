f619b3d6b0ffd23d71537a53fa7450599ee08f22
Huaping Gu <hpgu@apple.com>
2016-11-16 14:31:43 -0800

    add post to recerive message

--- src/main/java/gu/humphrey/Main.java
+++ src/main/java/gu/humphrey/Main.java
@@ -9,44 +9,51 @@ import java.util.Map;
 import java.util.Set;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
 import static spark.SparkBase.port;
 import static spark.SparkBase.staticFileLocation;
 
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    port(Integer.valueOf(args[0]));
+        port(Integer.valueOf(args[0]));
 
-    staticFileLocation("/public");
+        staticFileLocation("/public");
 
-    get("/hello", (req, res) -> "Hello World");
+        get("/hello", (req, res) -> "Hello World");
 
-      get("/hook", (req, res) -> {
-                  System.out.println("got request on /hook");
-                  if ("subscribe".equals(req.queryParams("hub.mode"))) {
-                      System.out.println("Validating hub.mode");
-                      if ("1234567890".equals(req.queryParams("hub.verify_token"))) {
-                          System.out.println("Validating passed");
-                          res.status(HttpServletResponse.SC_OK);
-                          return req.queryParams("hub.challenge");
-                      } else {
-                          System.out.println("Failed validation. Make sure the validation tokens match.");
-                          res.status(HttpServletResponse.SC_FORBIDDEN);
-                          return "token code not match challenge code";
-                      }
-                  }
+        get("/hook", (req, res) -> {
+                    System.out.println("got get on /hook");
+                    if ("subscribe".equals(req.queryParams("hub.mode")) &&
+                            ("1234567890".equals(req.queryParams("hub.verify_token")))) {
+                        System.out.println("Validating passed");
+                        res.status(HttpServletResponse.SC_OK);
+                        return req.queryParams("hub.challenge");
+                    } else {
+                        System.out.println("Failed validation. Make sure the validation tokens match.");
+                        res.status(HttpServletResponse.SC_FORBIDDEN);
+                        return "token code not match challenge code";
+                    }
 
-                  String body = req.body();
-                  System.out.println("body:"+body);
-
-                  return "body:"+body;
-              }
+                }
 
 
-      );
+        );
 
-    get("/", (request, response) -> {
+        post("/hook", (req, res) -> {
+                    System.out.println("got post on /hook");
+
+                    String body = req.body();
+                    System.out.println("body:" + body);
+
+                    return "body:" + body;
+                }
+
+
+        );
+
+        get("/", (request, response) -> {
             Map<String, Object> attributes = new HashMap<>();
             attributes.put("message", "Hello World!");
 
@@ -54,6 +61,6 @@ public class Main {
         }, new FreeMarkerEngine());
 
 
-  }
+    }
 
 }
